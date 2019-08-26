514459669b25f44a32af06ce2a37657f80ee53f3
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-08-26 13:11:18 -0700

    added post actions, what the heck is happening in delete post tho idk

---
+++ src/actions/postActions.js
@@ -0,0 +1,12 @@
+
+export const ADD_POST = 'ADD_POST';
+export const addPost = post => {
+    return { type: ADD_POST, payload: { id: post.id, title: post.title, body: post.body } };
+};
+
+
+//TODO: how do you fucking delete a post what the fuuuuuuuuuck
+export const DELETE_POST = 'DELETE_POST';
+export const deletePost = post => {
+    return { type: DELETE_POST, payload: {} };
+};
