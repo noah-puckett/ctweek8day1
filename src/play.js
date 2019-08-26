fd8f339b935d10a87dd15a1e0466e57738fb1a86
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-08-26 13:03:40 -0700

    lecture notes

--- src/play.js
+++ src/play.js
@@ -1,4 +1,18 @@
 import { createStore } from 'redux';
+import { 
+    ADD_CHIPS, 
+    addChip,
+    ADD_DRINKS,
+    addDrink, 
+    ADD_SANDWICHES,
+    addSandwich, 
+    REMOVE_CHIPS, 
+    removeChip,
+    REMOVE_DRINKS,
+    removeDrink, 
+    REMOVE_SANDWICHES, 
+    removeSandwich,
+} from './actions/lunchActions';
 
 const initialState = {
     drinks: 0,
@@ -6,19 +20,22 @@ const initialState = {
     sandwiches: 0
 };
 
+//SPREAD STATE OR YOU WILL REGRET YOUR LIFE. 'alter' state by dot notating into it and then incrementing from that value
+//
+
 function reducer(state = initialState, action) {
     switch(action.type) {
-        case 'ADD_DRINKS':
+        case ADD_DRINKS:
             return { ...state, drinks: state.drinks + 1 };
-        case 'REMOVE_DRINKS':
+        case REMOVE_DRINKS:
             return { ...state, drinks: state.drinks - 1 };
-        case 'ADD_CHIPS':
+        case ADD_CHIPS:
             return { ...state, chips: state.chips + 1 };
-        case 'REMOVE_CHIPS':
+        case REMOVE_CHIPS:
             return { ...state, chips: state.chips - 1 };
-        case 'ADD_SANDWICHES':
+        case ADD_SANDWICHES:
             return { ...state, sandwiches: state.sandwiches + 1 };
-        case 'REMOVE_SANDWICHES':
+        case REMOVE_SANDWICHES:
             return { ...state, sandwiches: state.sandwiches - 1 };
         default:
             return state;
@@ -27,43 +44,7 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-const ADD_DRINKS = {
-    type: 'ADD_DRINKS'
-};
-
-const REMOVE_DRINKS = {
-    type: 'REMOVE_DRINKS'
-};
-
-const ADD_CHIPS = {
-    type: 'ADD_CHIPS'
-};
-
-const REMOVE_CHIPS = {
-    type: 'REMOVE_CHIPS'
-};
-
-// const ADD_SANDWICHES = {
-//     type: 'ADD_SANDWICHES'
-// };
-
-// const REMOVE_SANDWICHES = {
-//     type: 'REMOVE_SANDWICHES'
-// };
-
-store.dispatch(ADD_DRINKS);
-store.dispatch(ADD_DRINKS);
-store.dispatch(ADD_DRINKS);
-store.dispatch(REMOVE_DRINKS);
-
-store.dispatch(ADD_CHIPS);
-store.dispatch(ADD_CHIPS);
-store.dispatch(ADD_CHIPS);
-store.dispatch(REMOVE_CHIPS);
-
-
-
-
+store.dispatch(addDrink(1));
 
 
 console.log(store.getState());
